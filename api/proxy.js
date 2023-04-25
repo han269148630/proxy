78718e5d097db821792c478a37abf24f4b5345b8
han269148630 <60659094+han269148630@users.noreply.github.com>
2023-04-25 23:34:33 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
   let target = "https://www.google.com";
-  // 代理目标地址
+  // 代理目标地址谷歌
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   //   if (
   //     req.url.startsWith("/api") ||
